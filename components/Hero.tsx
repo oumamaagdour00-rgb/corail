0d06ee22eecf1eb895634c4c03e551f3cc3b802f
soumaya-am <oumamaagdour00@gmail.com>
2026-03-28 16:39:08 +0100

    h

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -20,7 +20,7 @@ const Hero: React.FC = () => {
 	}, []);
 
 	return (
-		<div className="relative w-full overflow-hidden" style={{ height: 'calc(90vh - 85px)', minHeight: '500px' }}>
+		<div className="relative w-full overflow-hidden" style={{ height: 'calc(94vh - 85px)', minHeight: '500px' }}>
 			{/* Background Image - Corporate Building */}
 			<div
 				ref={bgRef}
